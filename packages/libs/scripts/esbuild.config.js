7d69d6af53b2b85ee4ffbf34971efe75b64002ef
apisit110 <apisitnowarat@gmail.com>
2024-08-13 21:45:41 +0700

    feat: update packages & build app

--- packages/libs/scripts/esbuild.config.js
+++ packages/libs/scripts/esbuild.config.js
@@ -3,5 +3,20 @@ module.exports = {
     outdir: 'dist',
     bundle: true,
     minify: false
-  }
+  },
+  prebuild: async () => {
+    console.log("prebuild")
+    const { sync } = require('rimraf')
+    sync("./dist") // clean up dist folder
+  },
+  postbuild: async () => {
+    console.log("postbuild")
+    // const cpy = (await import("cpy")).default
+    // await cpy(
+    //   [
+    //     "../../sequelize"
+    //   ],
+    //   "dist"
+    // )
+  },
 }
