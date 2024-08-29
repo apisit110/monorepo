f780927a96af910fc3ed70057a0ae7718815be99
apisit110 <apisitnowarat@gmail.com>
2024-08-29 14:38:53 +0700

    feat: update build and rn golang service

--- apps/mylab-golang-service/libs/utils/helper/orm.go
+++ apps/mylab-golang-service/libs/utils/helper/orm.go
@@ -1,8 +1,10 @@
 package helper
 
 import (
+	"os"
 	"mylab-golang-service/libs/model"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -12,7 +14,21 @@ var Db *gorm.DB
 var err error
 
 func Init() {
-	dsn := "root:1qazZAQ!@tcp(127.0.0.1:3306)/mylab?charset=utf8mb4&parseTime=True&loc=Local"
+	env := os.Getenv("ENVIRONMENT")
+	if env != "DEV" {
+		err := godotenv.Load("../../packages/libs/.env")
+		if err != nil {
+			panic("unable to load .env")
+		}
+	}
+
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+
+	dsn := dbUsername + ":" + dbPassword + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
 	Db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
